a4e99a1eaaa7ea2fc078a28884eee065f31cfa12
Robert Hoffmann <rob.hoffmann@posteo.de>
2023-12-31 17:29:20 +0100

    Refactor control.js and add event listeners

--- e_rezept_fachdienst/static/js/control.js
+++ e_rezept_fachdienst/static/js/control.js
@@ -1,80 +1,72 @@
-import {addReceiptItem } from "./receiptManager.js";
-import { getPrescriptionDefaultData, getDispenseDefaultData, gatherSentPrescriptionFromInputFields,  gatherDispensationFromPrescriptionInputFields, gatherDispensationFromDispensationInputFields } from "./receiptDataManager.js";
-import { addOrganisation, getDefaultDoctorOrganisationData,getDefaultFarmacyOrganisationData  } from "./organisationManager.js";
+import { addReceiptItem } from "./receiptManager.js";
+import { getPrescriptionDefaultData, getDispenseDefaultData, gatherSentPrescriptionFromInputFields, gatherDispensationFromPrescriptionInputFields, gatherDispensationFromDispensationInputFields } from "./receiptDataManager.js";
+
+import { addOrganisation, getDefaultDoctorOrganisationData, getDefaultFarmacyOrganisationData } from "./organisationManager.js";
+
 import { addPractitioner } from "./practitionerManager.js";
 import { makeRequest, collectPrescriptionRequestData, collectDispensationRequestData } from "./requestManager.js";
 
-document.getElementById('addPrescription').addEventListener('click', function() {
-    addReceiptItem('prescriptionAccordion', getPrescriptionDefaultData());
-});
+function setupEventListeners() {
+    document.getElementById('addPrescription').addEventListener('click', () => {
+        addReceiptItem('prescriptionAccordion', getPrescriptionDefaultData());
+    });
 
-document.getElementById('addDispensation').addEventListener('click', function() {
-    addReceiptItem('dispensationAccordion', getDispenseDefaultData());
-});
+    document.getElementById('addDispensation').addEventListener('click', () => {
+        addReceiptItem('dispensationAccordion', getDispenseDefaultData());
+    });
 
+    $('#sendPrescription').click(sendPrescription);
+    $('#cancelPrescription').click(cancelPrescription);
+    $('#sendDispensation').click(sendDispensation);
+    $('#cancelDispensation').click(cancelDispensation);
+}
 
-addReceiptItem('prescriptionAccordion',getPrescriptionDefaultData());
+function sendPrescription() {
+    const requestData = collectPrescriptionRequestData();
+    const requestPayload = { prescriptions: requestData };
 
+    makeRequest('/send_prescription', 'Prescription sent', requestPayload)
+        .then(response => handlePrescriptionResponse())
+        .catch(error => console.error("Error sending prescription:", error));
+}
 
+function handlePrescriptionResponse() {
+    processDispensations(gatherDispensationFromPrescriptionInputFields(), 'dispensationAccordion');
+    processPrescriptions(gatherSentPrescriptionFromInputFields(), 'sentPrescriptionAccordion');
+}
+
+function cancelPrescription() {
+    const selectedIds = getSelectedItems('.checkbox-button:checked[data-purpose="sentPrescription"]', 'data-rxPrescriptionProcessIdentifier');
+    makeRequest('/cancel_prescription', 'Prescription cancelled', { prescriptionIdentifiers: selectedIds });
+}
+
+function sendDispensation() {
+    const requestData = collectDispensationRequestData();
+    makeRequest('/send_dispensation', 'Dispensation sent', { dispensations: requestData })
+        .then(response => processDispensations(gatherDispensationFromDispensationInputFields(), 'sentDispensationAccordion'));
+}
+
+function cancelDispensation() {
+    const selectedIds = getSelectedItems('.checkbox-button:checked[data-purpose="sentDispensation"]', 'data-rxPrescriptionProcessIdentifier');
+    makeRequest('/cancel_dispensation', 'Dispensation cancelled', { dispensationIdentifiers: selectedIds });
+}
+
+function processDispensations(dispensations, accordionId) {
+    dispensations.forEach(dispensation => addReceiptItem(accordionId, dispensation));
+}
+
+function processPrescriptions(prescriptions, accordionId) {
+    prescriptions.forEach(prescription => addReceiptItem(accordionId, prescription));
+}
+
+function getSelectedItems(selector, dataAttribute) {
+    return Array.from(document.querySelectorAll(selector)).map(checkbox => ({
+        rxPrescriptionProcessIdentifier: checkbox.getAttribute(dataAttribute)
+    }));
+}
+
+addReceiptItem('prescriptionAccordion', getPrescriptionDefaultData());
 addOrganisation(getDefaultDoctorOrganisationData());
 addOrganisation(getDefaultFarmacyOrganisationData());
 addPractitioner();
-
-$('#sendPrescription').click(function () {
-    let requestData = collectPrescriptionRequestData();
-    let requestPayload = { prescriptions: requestData };
-
-    makeRequest('/send_prescription', 'Prescription sent', requestPayload)
-    .then(response => {
-
-        let dispensations = gatherDispensationFromPrescriptionInputFields();
-        for (const dispensation of dispensations) {
-            addReceiptItem('dispensationAccordion', dispensation);
-        }
-        let prescriptions = gatherSentPrescriptionFromInputFields()
-        for (const prescription of prescriptions) {
-            addReceiptItem('sentPrescriptionAccordion', prescription);
-        }
-        console.log("Prescriptions sent:", prescriptions);
-    })
-    .catch(error => {
-        console.error("Error sending prescription:", error);
-    });
-});
-
-
-
-$('#cancelPrescription').click(function () {
-    const checkedCheckboxes = document.querySelectorAll('.checkbox-button:checked[data-purpose="sentPrescription"]');
-    const selectedIds = Array.from(checkedCheckboxes).map(checkbox => {
-        return { 'rxPrescriptionProcessIdentifier': checkbox.getAttribute('data-rxPrescriptionProcessIdentifier') };
-    });
-    let requestPayload = { prescriptionIdentifiers: selectedIds };
-    makeRequest('/cancel_prescription', 'Prescription cancelled', requestPayload);
-});
-
-
-// Send dispensation button click event
-$('#sendDispensation').click(function () {
-    let requestData = collectDispensationRequestData();
-    let requestPayload = { dispensations: requestData };
-    console.log("requestPayload:", requestPayload);
-    makeRequest('/send_dispensation', 'Dispensation sent', requestPayload).then(response => {
-        let dispensations = gatherDispensationFromDispensationInputFields();
-        for (const dispensation of dispensations) {
-            addReceiptItem('sentDispensationAccordion', dispensation);
-        }
-    })
-});
-
-// Cancel dispensation button click event
-$('#cancelDispensation').click(function () {
-    const checkedCheckboxes = document.querySelectorAll('.checkbox-button:checked[data-purpose="sentDispensation"]');
-    const selectedIds = Array.from(checkedCheckboxes).map(checkbox => {
-        // Modify this line according to the data format expected by /cancel_dispensation
-        return { 'rxPrescriptionProcessIdentifier': checkbox.getAttribute('data-rxPrescriptionProcessIdentifier') };
-    });
-    let requestPayload = { dispensationIdentifiers: selectedIds };
-    makeRequest('/cancel_dispensation', 'Dispensation cancelled', requestPayload);
-});
-
+setupEventListeners();
