e84be5ccee706de44c9dcc62238fbd340a219bfd
Robert Hoffmann <rob.hoffmann@posteo.de>
2023-12-31 17:31:41 +0100

    Refactor logManager.js to improve readability and maintainability

--- e_rezept_fachdienst/static/js/logManager.js
+++ e_rezept_fachdienst/static/js/logManager.js
@@ -1,58 +1,59 @@
-
+const MAX_LOG_ENTRIES = 10;
 
 let logStorage = [];
+
 function addLogEntry(action, url, requestPayload, response) {
     const timestamp = new Date().toISOString();
-
     logStorage.unshift({ timestamp, action, url, requestPayload, response });
 
-    if (logStorage.length > 10) {
+    if (logStorage.length > MAX_LOG_ENTRIES) {
         logStorage.pop();
     }
+
     updateLogDisplay();
 }
 
 function updateLogDisplay() {
-    let logTable = $('#logTableBody');
-    logTable.html('');
+    const logTable = $('#logTableBody');
+    logTable.empty();
 
     logStorage.forEach((log, index) => {
-        let isSuccess = log.response.status_code === 200;
-        let rowClass = isSuccess ? ' class="success-row"' : (log.response.status_code && log.response.status_code !== 200) ? ' class="error-row"' : '';
-
-        let payloadAccordion = `
-            <button class="btn-link collapsed" id="accordionButton${index}">Show Payload</button>
-            <div class="accordion-content" id="accordionPayload${index}" style="display: none;">
-                <pre>${JSON.stringify(log.requestPayload, null, 2)}</pre>
-            </div>
-        `;
-
-        let row = `<tr${rowClass}>
-            <td>${log.timestamp}</td>
-            <td>${log.action}</td>
-            <td>${log.url}</td>
-            <td>${payloadAccordion}</td>
-            <td>${log.response.message}</td>
-            <td>${log.response.status_code}</td>
-        </tr>`;
-
-        logTable.append(row);
+        const rowHtml = createLogTableRow(log, index);
+        logTable.append(rowHtml);
 
         // Attach event listener to the button
-        $(`#accordionButton${index}`).on('click', function() {
-            toggleAccordion(index);
-        });
+        $(`#accordionButton${index}`).click(() => toggleAccordion(index));
     });
 }
 
-function toggleAccordion(index) {
-    let accordionContent = document.getElementById(`accordionPayload${index}`);
-    accordionContent.style.display = accordionContent.style.display === "none" ? "block" : "none";
+function createLogTableRow(log, index) {
+    const isSuccess = log.response.status_code === 200;
+    const rowClass = isSuccess ? 'success-row' : (log.response.status_code && log.response.status_code !== 200) ? 'error-row' : '';
+
+    const payloadAccordionHtml = createPayloadAccordionHtml(log.requestPayload, index);
+
+    return `
+        <tr class="${rowClass}">
+            <td>${log.timestamp}</td>
+            <td>${log.action}</td>
+            <td>${log.url}</td>
+            <td>${payloadAccordionHtml}</td>
+            <td>${log.response.message}</td>
+            <td>${log.response.status_code}</td>
+        </tr>`;
 }
 
+function createPayloadAccordionHtml(requestPayload, index) {
+    return `
+        <button class="btn-link collapsed" id="accordionButton${index}">Show Payload</button>
+        <div class="accordion-content" id="accordionPayload${index}" style="display: none;">
+            <pre>${JSON.stringify(requestPayload, null, 2)}</pre>
+        </div>`;
+}
 
+function toggleAccordion(index) {
+    const accordionContent = $(`#accordionPayload${index}`);
+    accordionContent.toggle();
+}
 
-
-
-
-export { addLogEntry, updateLogDisplay }
+export { addLogEntry, updateLogDisplay };
